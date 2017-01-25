5651972659b58202c8497d509e7f564526846d43
Jonas Molina Ramirez <jm18magi@clientmaster.rbg.informatik.tu-darmstadt.de>
2017-01-25 12:51:20 +0100

    Added 4b)

---
+++ aufgabe_4_b.m
@@ -0,0 +1,45 @@
+
+%%
+clc;
+clear;
+n = 3;
+eps = 0.1;
+
+% %%
+% [A,b,c,B]=KleeMintyAlt(n,eps);
+% primalSimplex(A,b,c,B);
+% 
+% %%
+% [A,b,c,B]=KleeMintyBlock(n,eps);
+% primalSimplex(A,b,c,B);
+
+%% 4 b iii)
+[A,b,c,B]=KleeMintyAlt(n,eps);
+[Bx,xB,message,iter] = phase1(A,b,c);
+primalSimplex(A,b,c,Bx);
+
+%% 4 c)
+
+clc;
+clear;
+eps = 0.4;
+
+iterations_alt = zeros(1,15);
+iterations_block = zeros(1,15);
+
+for n = 1:15
+    [A,b,c,B]=KleeMintyAlt(n,eps);
+    [Bx,xB,message,iter]= primalSimplex(A,b,c,B);
+    iterations_alt(n) = iter + 1; 
+    [A,b,c,B]=KleeMintyBlock(n,eps); 
+    [Bx,xB,message,iter] = primalSimplex(A,b,c,B);
+    iterations_block(n) = iter + 1;
+end
+
+'Iterations alt:'
+iterations_alt
+
+
+'Iterations block:'
+iterations_block
+
