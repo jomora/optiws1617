fdc3107c6ada002c5f2c454908983fa6ba341209
Jonas Molina Ramirez <jm18magi@clientmaster.rbg.informatik.tu-darmstadt.de>
2017-01-26 17:42:39 +0100

    removed increment of iteration from aufgabe 4b) script

--- aufgabe_4_b.m
+++ aufgabe_4_b.m
@@ -1,19 +1,22 @@
-
 %%
 clc;
 clear;
 n = 3;
 eps = 0.1;
 
-% %%
-% [A,b,c,B]=KleeMintyAlt(n,eps);
-% primalSimplex(A,b,c,B);
+[A,b,c,B]=KleeMintyAlt(n,eps);
+primalSimplex(A,b,c,B);
 % 
 % %%
 % [A,b,c,B]=KleeMintyBlock(n,eps);
 % primalSimplex(A,b,c,B);
 
 %% 4 b iii)
+clc;
+clear;
+n = 3;
+eps = 0.1;
+
 [A,b,c,B]=KleeMintyAlt(n,eps);
 [Bx,xB,message,iter] = phase1(A,b,c);
 primalSimplex(A,b,c,Bx);
@@ -30,10 +33,10 @@ iterations_block = zeros(1,15);
 for n = 1:15
     [A,b,c,B]=KleeMintyAlt(n,eps);
     [Bx,xB,message,iter]= primalSimplex(A,b,c,B);
-    iterations_alt(n) = iter + 1; 
+    iterations_alt(n) = iter; 
     [A,b,c,B]=KleeMintyBlock(n,eps); 
     [Bx,xB,message,iter] = primalSimplex(A,b,c,B);
-    iterations_block(n) = iter + 1;
+    iterations_block(n) = iter;
 end
 
 'Iterations alt:'
